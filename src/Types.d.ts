345e01b7292d24f2a3aa3655353a494f717667d5
Vadim Kasatkin <penguin2804@gmail.com>
2021-08-10 18:26:27 +0400

    app start

---
+++ src/Types.d.ts
@@ -0,0 +1,7 @@
+declare interface Messsage {
+  date: Date,
+  message: string,
+  name: string
+}
+
+declare type MessageList = Messsage[];
---
+++ src/store.ts
@@ -0,0 +1,35 @@
+import { createStore } from "redux"
+
+interface State {
+  mesages: MessageList
+  name: string
+}
+
+interface Action {
+  type: string,
+  payload?: any
+}
+
+function chatReducer(state: State = {name: '', mesages: []}, action: Action): State {
+  switch (action.type) {
+    case 'PUSH_MESSAGES':
+      return {
+        ...state,
+        mesages: [...state.mesages, ...action.payload]
+      }
+    case 'PUSH_MESSAGE':
+      return {
+        ...state,
+        mesages: [...state.mesages, action.payload]
+      }
+    case 'SET_NAME':
+      return {
+        ...state,
+        name: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export const store = createStore(chatReducer)
