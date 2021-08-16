beb31f1fa69416426410e6c275398b395a7e11a6
Vadim Kasatkin <penguin2804@gmail.com>
2021-08-16 22:48:12 +0400

    fix tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -3,13 +3,13 @@ import { smileReplace } from "./utils";
 describe("utils test", () => {
   it("smile replace return with replace smile", () => {
     expect(smileReplace("some text :-)")).toBe(
-      "some text <img class=\"chat-message__smile\" src='img/smile.png'>"
+      "some text <img class=\"chat-message__smile\" src='img/smile.svg'>"
     );
     expect(smileReplace("some text :-(")).toBe(
-      "some text <img class=\"chat-message__smile\" src='img/sad.png'>"
+      "some text <img class=\"chat-message__smile\" src='img/sad.svg'>"
     );
     expect(smileReplace("some text :-D")).toBe(
-      "some text <img class=\"chat-message__smile\" src='img/laugh.png'>"
+      "some text <img class=\"chat-message__smile\" src='img/laugh.svg'>"
     );
   });
   it("smile replace return previos text without smile", () => {
